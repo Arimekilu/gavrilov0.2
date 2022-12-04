f933cb994ba7e372ebf009801b5227dbe079c113
Arimekilu <95185397+Arimekilu@users.noreply.github.com>
2022-12-04 22:39:23 +0200

    Front логика готова

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,19 +1,23 @@
-import { NgStyle } from '@angular/common';
-import { Component, OnInit, Output } from '@angular/core';
-import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
-import { transaction } from '../app.component';
-import { DataService } from '../data.service';
+import {NgStyle} from '@angular/common';
+import {Component, OnInit, Output} from '@angular/core';
+import {Form, FormControl, FormGroup, Validators} from '@angular/forms';
+import {BehaviorSubject, map, Observable, pipe, Subscription} from 'rxjs';
+import {DataService} from '../services/data.service';
+import {arrPayments} from "../data/arrPayments";
+import {PayDay, Payment} from "../interfaces/paymentInterface";
+
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
 })
-export class InputComponent implements OnInit {  
+export class InputComponent implements OnInit {
+  arrPaydays$: Observable<PayDay[]> = this.dataService.getData()
 
   myForm: FormGroup;
-  
-    constructor(public dataService: DataService) {
+
+  constructor(public dataService: DataService) {
     this.myForm = new FormGroup({
       value: new FormControl('', Validators.required),
       type: new FormControl('', Validators.required),
@@ -23,16 +27,65 @@ export class InputComponent implements OnInit {
   }
 
   submit() {
-    this.dataService.setData(this.myForm.value)
-    
+    // this.dataService.setPayDay(this.getPayDay())
+    console.log(!!this.foundedPayDay())
+    console.log(this.foundedPayDay())
+    if (!!this.foundedPayDay()) {
+      // @ts-ignore
+      this.dataService.setPayment(this.foundedPayDay(), (this.getPayment()))
+    } else {
+      this.dataService.setPayDay(this.getPayDay())
+    }
+
+    // let payDay: PayDay = this.getPayDay()
+    // payDay.payments.push()
+
     this.myForm.reset();
   }
 
-  
+  log() {
+    console.log(this.foundedPayDay())
+  }
 
-  ngOnInit(): void {}
+  getPayDay() {
+    return <PayDay>{
+      date: this.myForm.value.date,
+      payments: [{
+        value: this.myForm.value.type === '+' ? this.myForm.value.value : -this.myForm.value.value,
+        comment: this.myForm.value.comment,
+        date: new Date(this.myForm.value.date)
+      }]
+    }
+  }
+
+  getPayment() {
+    return <Payment>{
+      value: this.myForm.value.type === '+' ? this.myForm.value.value : -this.myForm.value.value,
+      comment: this.myForm.value.comment,
+      date: new Date(this.myForm.value.date)
+    }
+  }
+
+//   foundedPayDay(): PayDay => {
+//
+// }
+
+
+  foundedPayDay() {
+    let payDay
+    this.arrPaydays$.pipe(
+      map(value => {
+        return value.find(res => this.dataService.getRussianDate(res.date) === this.dataService.getRussianDate(this.myForm.value.date))
+      })
+    ).subscribe(val => {
+      payDay = val
+    })
+    return payDay
+  }
+
+  ngOnInit(): void {
+  }
 
   ngDoCheck() {
-    
   }
 }
--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -1,22 +1,52 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {transaction} from "../app.component";
-import { DataService } from '../data.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {map} from 'rxjs';
+import {Payment} from '../interfaces/paymentInterface';
+import {DataService} from '../services/data.service';
+
 
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
-  styleUrls: ['./output.component.scss']
+  styleUrls: ['./output.component.scss'],
 })
-
 @Input()
-
 export class OutputComponent implements OnInit {
+  dateForm: FormGroup;
 
   arrPay = this.dataService.getData()
 
-  constructor(public dataService: DataService) { }
+  logDate() {
+
+
+  }
+
+
+
+  // public calculateDate (d1: Date, d2: Date) {
+  //   const d1Days: number = d1.getDay();
+  //   const d1Month: number = d1.getMonth();
+  //   const d1Year: number = d1.getFullYear();
+  //
+  //   const d2Days: number = d2.getDay();
+  //   const d2Month: number = d2.getMonth();
+  //   const d2Year: number = d2.getFullYear();
+  //
+  //   return d1Days === d2Days && d1Month === d2Month && d1Year === d2Year;
+  // }
+
+  public getPayDaySum(Payment: Payment[]) {
+    return Payment.reduce((sum, current) => sum + current.value, 0)
+  }
+
+  constructor(public dataService: DataService) {
+    this.dateForm = new FormGroup({
+      date: new FormControl(new Date(), Validators.required),
+    });
+  }
 
   ngOnInit() {
   }
+
 
 }
